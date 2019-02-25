2b1d919a36cfafffc63c278350b74d170d9a4287
Arun-Sirimalla <37253554+Arun-Sirimalla@users.noreply.github.com>
2019-02-25 06:55:54 -0600

    1
    1

---
+++ redirect.js
@@ -0,0 +1,7 @@
+﻿<script type="text/javascript">
+    function preloadFunc()
+    {
+        window.location.replace("https://i81.sharepoint.com/sites/ilab/SitePages/Home.aspx");
+    }
+    window.onpaint = preloadFunc();
+</script>
